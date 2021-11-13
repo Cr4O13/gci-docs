a6b06361545a8d6b908cf26e6055f3dc10c27533
Cr4O13 <tetrachromat@outlook.com>
2021-11-13 19:03:59 +0100

    add responder logging
    add logging to responder functions,
    remove obsolete code

--- src/model/responder.lua
+++ src/model/responder.lua
@@ -11,26 +11,48 @@
   end
 
   local function fs2020_write(self, input)
-    fs2020_variable_write( self.var_id, self.unit_id, self.output(input) )
+    local variable = self.var_id
+    local unit = self.unit_id
+    local value = self.output(input)
+    self:log_event("INFO", "write", "fs2020_variable_write", variable, unit, value )
+    fs2020_variable_write( variable, unit, value )
   end
 
   local function fs2020_send(self, input)
-    fs2020_event( self.var_id, self.output(input) )
+    local event = self.var_id
+    local value = self.output(input)
+    self:log_event("INFO", "send", "fs2020_event", event, value )
+    fs2020_event( event, value )
   end
 
   local function fsx_write(self, input)
-    fsx_variable_write( self.var_id, self.unit_id, self.output(input) )
+    local variable = self.var_id
+    local unit = self.unit_id
+    local value = self.output(input)
+    self:log_event("INFO", "write", "fsx_variable_write", variable, unit, value )
+    fsx_variable_write( variable, unit, value )
   end
 
   local function fsx_send(self, input)
-    fsx_event( self.var_id, self.output(input) )
+    local event = self.var_id
+    local value = self.output(input)
+    self:log_event("INFO", "send", "fsx_event", event, value )
+    fsx_event( event, value )
   end
 
   local function xpl_write(self, input)
-    xpl_dataref_write( self.var_id, self.unit_id, self.output(input), self.offset, self.force )
+    local dataref = self.var_id
+    local type    = self.unit_id
+    local value   = self.output(input)
+    local offset  = self.offset
+    local force   = self.force
+    self:log_event("INFO", "xplwrite", "xpl_dataref_write", dataref, type, value, offset, force  )
+    xpl_dataref_write( dataref, type, value, offset, force )
   end
 
   local function xpl_send(self, input)
+    local commandref = self.var_id
+    self:log_event("INFO", "xplsend", "xpl_command", commandref )
     xpl_command( self.var_id )
   end
 
@@ -63,7 +85,11 @@
 -- ---------------------------------------------------------
   local gci_responder = gci_base:new {
     events = {
-      respond  = "responds with '%s'('%s', '%s', %s)",
+      send     = "responds with %s('%s', '%s')",
+      xplsend  = "responds with %s('%s')",
+      write    = "responds with %s('%s', '%s', %s)",
+      xplwrite = "responds with %s('%s', '%s', %s, '%s', %s)",
+      publish  = "responds with %s('%s', '%s', %s)",
       missing  = "respond method missing!"
     }
   }
